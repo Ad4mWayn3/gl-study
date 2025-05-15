476a4ceff4beb2ae10be3b4704faa150f32465b5
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-05-15 08:16:21 -0500

    Shaders now sample and mix textures with vertex colors

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,8 +1,12 @@
 #version 330 core
 in vec3 vertColor;
+in vec2 texCoord;
+
 out vec4 FragColor;
-uniform float redValue;
+
+uniform sampler2D tex;
 
 void main() {
-	FragColor = vec4(vertColor, 1.0f);
+	vec4 texColor = texture(tex, texCoord);
+	FragColor = mix(texColor, vec4(vertColor, 1.), 0.4);
 }
