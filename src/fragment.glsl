171785d5fd41769ea728b0d78ce1dc1690cf86c1
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-05-19 07:31:30 -0500

    fragment shader now mixes two textures over time

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -5,8 +5,12 @@ in vec2 texCoord;
 out vec4 FragColor;
 
 uniform sampler2D tex;
+uniform sampler2D tex2;
+uniform float redValue;
 
 void main() {
-	vec4 texColor = texture(tex, texCoord);
-	FragColor = mix(texColor, vec4(vertColor, 1.), 0.4);
+	vec4 texColor1 = texture(tex, texCoord);
+	vec4 texColor2 = texture(tex2, texCoord);
+
+	FragColor = mix(texColor1, texColor2, redValue);
 }
