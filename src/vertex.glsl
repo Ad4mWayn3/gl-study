198637b2b482e22c0a7e5f6d60ad780afca35f4a
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-06-08 21:58:52 -0500

    formatting

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -4,6 +4,7 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform float redValue;
+uniform vec3 camera;
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
@@ -13,7 +14,6 @@ out vec2 texCoord;
 
 void main() {
 	mat4 transform = projection * view * model;
-//	transform = (transform * (1.0 - redValue)) + (mat4(1.) * redValue);
 
 	gl_Position = transform * (vec4(aPos, 1.0) + vec4(-.3, -.3, 0., 0.));
 	texCoord = aTexCoord;
