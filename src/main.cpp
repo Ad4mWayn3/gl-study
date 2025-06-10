f9312e479245d97f0e0cbc5fab2ed3119df7ffe1
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-06-10 08:58:51 -0500

    renderer class pt2

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,22 @@
-// #include "shader.hpp"
-// #include "texture.hpp"
-// #include "glm.hpp"
+#include "renderer.hpp"
 
-// #include <glad/glad.h>
-// #include <GLFW/glfw3.h>
-// #include <stb_image.h>
-
-#include <algorithm>
-#include <cassert>
-#include <cstdio>
-#include <cstring>
 #include <iostream>
-
-template <class T>
-struct Tree {
-	bool hollow=false;
-	T value;
-	Tree* left=nullptr;
-	Tree* right=nullptr;
-	Tree(T&& _value, Tree&& _left, Tree&& _right)
-	:	value{_value}{
-		left = new Tree{(Tree&&)_left};
-		right = new Tree{(Tree&&)_right};
-	}
-	Tree(const Tree& tree) : value{tree.value}, left{nullptr}, right{nullptr}{
-		std::cout << "Tree(const&)\n";
-		if (tree.left) {
-			assert (tree.left != nullptr);
-			left = new Tree(*(tree.left));
-		} if (tree.right) {
-			right = new Tree(*(tree.right));
-		}
-	}
-	Tree(Tree& tree) : this{&tree}{
-		std::cout << "Tree(&)\n";
-	}
-	Tree(Tree&& tree) : value{tree.value}, left{tree.left}, right{tree.right}{
-		tree.hollow = true;
-		tree.left = nullptr;
-		tree.right = nullptr;
-	}
-	Tree() : value{}, left{nullptr}, right{nullptr}{
-	}
-	~Tree() {
-		if (hollow) return;
-		if (left){
-			delete left;
-			left = nullptr;
-		} if (right) {
-			delete right;
-		}
-	}
-	Tree& get_left() {
-		return *left;
-	}
-	Tree& get_right() {
-		return *right;
-	}
-	Tree& insert(T&& value) {
-		for (auto i=this;i != nullptr;) {
-			if (value >= i->value)
-		}
-	}
-};
+#define LOG(...) printf(__VA_ARGS__)
 
 int main() {
-	auto& tree = Tree<int>(12, {}, {})
-		.insert(21)
-		.insert(34)
-		.insert(32);
+	auto window = GLFWwindow_create(1920, 1080, "gl study!");
+	if (!window) {
+		LOG("Failed to create window\n");
+		return -1;
+	}
+	auto r = Renderer::init(window);
+	Seconds currTime = glfwGetTime();
+	Seconds delta = 0;
+	while (!glfwWindowShouldClose(window)) {
+		delta = glfwGetTime() - currTime;
+		currTime = glfwGetTime();
+		r.process(delta, glm::vec4(.6f, .6f, .6f, 1.f));
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 }
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -6,6 +6,12 @@
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
 
+#include <vector>
+
+#ifndef LOG(...)
+#define LOG(...)
+#endif
+
 using Seconds = float;
 
 struct Camera {
@@ -34,20 +40,77 @@ template <class T>
 struct Uniform {
 	GLuint id;
 	T data;
-	static Uniform create(T&& p_data, GLuint program, const char* name) {
-		data = p_data;
-		id = glGetUniformLocation(program, name);
+	static Uniform create(T&& data, GLuint program, const char* name) {
+		Uniform out;
+		out.data = data;
+		out.id = glGetUniformLocation(program, name);
+		return out;
+	}
+	static Uniform assign(T&& data, GLuint id) {
+		Uniform out;
+		out.data = data;
+		out.id = id;
+		return out;
+	}
+};
+
+struct Mesh {
+	std::vector<float> vertices;
+	std::vector<int> indices;
+	GLsizei stride;
+	GLuint VBO, VAO, EBO;
+	static Mesh create(std::vector<float>&& vertices,
+		std::vector<int>&& indices) {
+		Mesh out;
+		out.vertices = vertices;
+		out.indices = indices;
+		out.stride = 5 * sizeof(float);
+		
+		GLuint VBO=0, EBO=0, VAO=0;
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
+
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(out.vertices), out.vertices.data(),
+			GL_DYNAMIC_DRAW);
+		assert(sizeof(out.vertices) == 
+			out.vertices.size() * sizeof(out.vertices[0]));
+
+		glGenBuffers(1, &EBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(out.indices),
+			out.indices.data(), GL_DYNAMIC_DRAW);
+		assert(sizeof(out.indices) ==
+			out.indices.size() * sizeof(out.indices[0]));
+
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+		glEnableVertexAttribArray(2);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, out.stride,
+			(GLvoid*)0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, out.stride,
+			(GLvoid*)(3 * sizeof(float)));
+		out.VBO = VBO;
+		out.VAO = VAO;
+		out.EBO = EBO;
+		return out;
 	}
 };
 
 struct Renderer {
 	Uniform<Camera> cameraU;
 	Uniform<glm::mat4> model, view, projection;
+	Uniform<Texture> trollcake, derpina;
+	Uniform<float> redValue;
+	Mesh mesh;
+	GLFWwindow* window;
 	GLuint VBO, EBO, VAO;
 	ShaderProgram program;
 
-	static Renderer init(glm::ivec2 resolution);
-	Renderer process(Seconds delta);
+	static Renderer init(GLFWwindow* window);
+	void process(Seconds delta, glm::vec4 clearColor);
 };
 
 void frameBufferResize(GLFWwindow* window, int width, int height) {
@@ -80,8 +143,13 @@ GLFWwindow* GLFWwindow_create(int width, int height, const char* title){
 	return window;
 }
 
-Renderer Renderer::init(glm::ivec2 resolution) {
-	float square[] = {
+Renderer Renderer::init(GLFWwindow* window) {
+	glEnable(GL_DEPTH_TEST);
+	int w=0, h=0;
+	glfwGetWindowSize(window, &w, &h);
+	auto resolution = glm::vec2((float)w, (float)h);
+
+	auto vertices = std::vector{
 		// front vertices
 		-.2f, -.2f, 0.f,	0.0f, 0.0f, // bottom left
 		.8f,  -.2f, 0.f,	1.0f, 0.0f, // bottom right
@@ -94,7 +162,7 @@ Renderer Renderer::init(glm::ivec2 resolution) {
 		-.2f,  .8f, 1.f,	1.0f, 0.0f, // top left
 		.8f,   .8f, 1.f,	0.0f, 0.0f, // top right
 	};
-	int squareIdxs[] = {
+	auto indices = std::vector{
 		//front
 		0, 1, 2,
 		1, 2, 3,
@@ -114,36 +182,7 @@ Renderer Renderer::init(glm::ivec2 resolution) {
 		1, 3, 5,
 		3, 5, 7,
 	};
-	GLsizei stride = 5 * sizeof(float);
-#pragma region gpu vertex config
-	GLuint VBO=0, EBO=0, VAO=0;
-
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(square), square,
-		GL_DYNAMIC_DRAW);
-
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIdxs), squareIdxs,
-		GL_DYNAMIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride,
-		(GLvoid*)0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride,
-		(GLvoid*)(3 * sizeof(float)));
-	//	unbind the array and attrib array, required if objects with different
-	//  attributes will be defined
-	//	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	//	glBindVertexArray(0);
-#pragma endregion
+	auto cube = Mesh::create(std::move(vertices), std::move(indices));
 	
 	ShaderProgram program = ShaderProgram::buildPath("src/vertex.glsl",
 		"src/fragment.glsl");
@@ -198,7 +237,6 @@ Renderer Renderer::init(glm::ivec2 resolution) {
 	auto view = glm::lookAt(camera.data.pos,
 		camera.data.getTarget(),
 		glm::vec3(0.f, 1.f, 0.f));
-		//glm::lookAt( camera.pos, camera.getTarget(), camera.up);
 	auto projection = glm::perspective(glm::radians(45.0f),
 		(float)resolution.x / resolution.y, 0.1f, 100.0f);
 	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
@@ -207,196 +245,40 @@ Renderer Renderer::init(glm::ivec2 resolution) {
 #pragma endregion
 	Renderer r;
 	r.cameraU = std::move(camera);
-	#define unif(x) Uniform<glm::mat4>::create(std::move(x), program.obj, #x);
-	r.model = unif(model);
-	r.view = unif(view);
-	r.projection = unif(projection);
+	r.window = window;
+	r.mesh = std::move(cube);
+	r.model = Uniform<glm::mat4>::assign(std::move(model), modelLoc);
+	r.view = Uniform<glm::mat4>::assign(std::move(view), viewLoc);
+	r.projection = Uniform<glm::mat4>::assign(std::move(projection), projectionLoc);
+	return r;
 }
 
-int r_main() {
-	struct { int w, h; } resolution {800, 600};
-	auto window = GLFWwindow_create(resolution.w, resolution.h, "gl study!");
-	if (!window) return -1;
-	glEnable(GL_DEPTH_TEST);
-	
-	float square[] = {
-		// front vertices
-		-.2f, -.2f, 0.f,	0.0f, 0.0f, // bottom left
-		.8f,  -.2f, 0.f,	1.0f, 0.0f, // bottom right
-		-.2f,  .8f, 0.f,	0.0f, 1.0f, // top left
-		.8f,   .8f, 0.f,	1.0f, 1.0f, // top right
+void Renderer::process(Seconds delta, glm::vec4 clearColor) {
+	const static auto id4x4 = glm::mat4(1.);
+	Seconds currTime = glfwGetTime();
+	model.data = glm::rotate(id4x4, currTime * glm::radians(-55.f),
+		glm::vec3(.5f, 1.f, 0.f));
+	glUniformMatrix4fv(model.id, 1, GL_FALSE, glm::value_ptr(model.data));
 
-		// back vertices
-		-.2f, -.2f, 1.f,	1.0f, 1.0f, // bottom left
-		.8f,  -.2f, 1.f,	0.0f, 1.0f, // bottom right
-		-.2f,  .8f, 1.f,	1.0f, 0.0f, // top left
-		.8f,   .8f, 1.f,	0.0f, 0.0f, // top right
-	};
-	int squareIdxs[] = {
-		//front
-		0, 1, 2,
-		1, 2, 3,
-		//back
-		4, 5, 6,
-		5, 6, 7,
-		//bottom
-		0, 1, 4,
-		1, 4, 5,
-		//top
-		2, 7, 3,
-		7, 2, 6,
-		//left
-		0, 2, 4,
-		2, 4, 6,
-		//right
-		1, 3, 5,
-		3, 5, 7,
-	};
-	GLsizei stride = 5 * sizeof(float);
-#pragma region gpu vertex config
-	GLuint VBO=0, EBO=0, VAO=0;
-
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(square), square,
-		GL_DYNAMIC_DRAW);
-
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIdxs), squareIdxs,
-		GL_DYNAMIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride,
-		(GLvoid*)0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride,
-		(GLvoid*)(3 * sizeof(float)));
-	//	unbind the array and attrib array, required if objects with different
-	//  attributes will be defined
-	//	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	//	glBindVertexArray(0);
-#pragma endregion
-	
-	ShaderProgram program = ShaderProgram::buildPath("src/vertex.glsl",
-		"src/fragment.glsl");
-	glUseProgram(program.obj);
-
-	{ // texture parameter config
-		stbi_set_flip_vertically_on_load(true);
-		Texture::setParameter(GL_TEXTURE_WRAP_S, GL_REPEAT);
-		Texture::setParameter(GL_TEXTURE_WRAP_T, GL_REPEAT);
-		Texture::setParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		Texture::setParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	}
-
-	auto trollcake = Texture::generate(GL_TEXTURE0)
-		.bind()
-		.loadFromPath("resources/trollcake.jpg")
-		.unbind();
-	auto derpina = Texture::generate(GL_TEXTURE1)
-		.bind()
-		.loadFromPath("resources/derpina.jpg")
-		.unbind();
-	
-	glUniform1i(glGetUniformLocation(program.obj, "tex"), 0);
-	glUniform1i(glGetUniformLocation(program.obj, "tex2"), 1);
-
-	struct { GLint id; float value; } redValue
-		{ glGetUniformLocation(program.obj, "redValue"), 0 };
-
-	float mixValue = 0.5f;
-	float delta = 0.f;
-	float currTime = 0.f;
-
-	auto zigzag = [](float x) -> float {
-		return 2. * glm::abs(x/2. - glm::floor(x/2. + 1./2.));
-	};
-
-	auto camera = Camera::fromTarget(glm::vec3(0.f, 0.f, 3.f),
-		glm::vec3(0.f, 0.f, 0.f));
-	GLuint cameraLoc = program.getUniformId("camera");
-	
-#pragma region matrices
-	const auto id4x4 = glm::mat4(1.);
-	
-	GLuint modelLoc = program.getUniformId("model"),
-		viewLoc = program.getUniformId("view"),
-		projectionLoc = program.getUniformId("projection");
-
-	auto model = glm::rotate(id4x4, currTime * glm::radians(-55.0f),
-		glm::vec3(0.5f,1.0f, 0.f));
-	auto view = glm::lookAt(camera.pos,
-		camera.getTarget(),
-		glm::vec3(0.f, 1.f, 0.f));
-		//glm::lookAt( camera.pos, camera.getTarget(), camera.up);
-	auto projection = glm::perspective(glm::radians(45.0f),
-		(float)resolution.w / resolution.h, 0.1f, 100.0f);
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-#pragma endregion
-	
-	while (!glfwWindowShouldClose(window)) {
-		glfwGetWindowSize(window, &resolution.h, &resolution.h);
-
-		delta = glfwGetTime() - currTime;
-		currTime += delta;
-
-		model = glm::rotate(id4x4, currTime * glm::radians(-55.f),
-			glm::vec3(.5f, 1.f, 0.f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-		// view = glm::lookAt(camera.pos, camera.getTarget(), camera.up);
-		// glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-			
-		glUniform1f(redValue.id, zigzag(mixValue));
+	// view = glm::lookAt(camera.pos, camera.getTarget(), camera.up);
+	// glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
 /*		auto vertexBuffer = (float*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-		vertexBuffer[25] = glm::sin(currTime * 2.)/2. + 1./2.;
-		vertexBuffer[8] = zigzag(currTime);
-		glUnmapBuffer(GL_ARRAY_BUFFER);
+	vertexBuffer[25] = glm::sin(currTime * 2.)/2. + 1./2.;
+	vertexBuffer[8] = zigzag(currTime);
+	glUnmapBuffer(GL_ARRAY_BUFFER);
 */
-		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-			mixValue += 1.f * delta;
-		} if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-			mixValue -= 1.f * delta;
-		} if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-			camera.pos.x -= 1.f * delta;
-		} if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
-			camera.pos.x += 1.f * delta;
-		}
-		
-		auto red = glm::vec3(1., 0., 0.);
-		auto blue = glm::vec3(0., 1., 0.);
-		auto clearColor = glm::mix(red, blue, zigzag(mixValue));
 
-		glClearColor(clearColor.r, clearColor.g, clearColor.b, 1.);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// bind if another object was drawn previously;
-		trollcake.bind();
-		derpina.bind();
+	// bind if another object was drawn previously;
+	trollcake.data.bind();
+	derpina.data.bind();
 //		glBindTexture(GL_TEXTURE_2D, id);
 //		glBindVertexArray(VAO);
 //		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 //		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glDrawElements(GL_TRIANGLES, std::size(squareIdxs), GL_UNSIGNED_INT, nullptr);
+	glDrawElements(GL_TRIANGLES, mesh.indices.size(), GL_UNSIGNED_INT, nullptr);
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-
-	glfwTerminate();
-	return 0;
 }
-
