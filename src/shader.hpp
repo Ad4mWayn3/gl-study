d104c5bfca22c23f5e175f97c8acd9d4f319fe40
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-06-11 20:25:59 -0500

    .

--- src/shader.hpp
+++ src/shader.hpp
@@ -6,26 +6,19 @@ std::string readShaderFile(const char* filePath);
 
 struct Shader {
 	GLuint obj;
-
 	static Shader build(GLenum type, const GLchar* const source);
-
 	~Shader();
 };
 
 struct ShaderProgram {
 	GLuint obj;
-
 	static ShaderProgram build(Shader vertex, Shader fragment);
-
 	GLuint getUniformId(const char* name);
-
 // preferred, it deletes the shaders immediately after returning the shader
 	// program.
 	static ShaderProgram buildSrc(const char* vertexShaderSource,
 		const char* fragmentShaderSource);
-
 	static ShaderProgram buildPath(const char* vertexShaderPath,
 		const char* fragmentShaderPath);
-
 	~ShaderProgram();
 };
