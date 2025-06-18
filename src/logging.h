acfc86c799942ccaeb7e331e21a201fe735d1a16
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-06-18 11:59:59 -0500

    .

--- src/logging.h
+++ src/logging.h
@@ -4,15 +4,15 @@
 
 inline const char* getErrorName(GLenum error) {
 	switch (error) {
-		#define emap(x) case x: return #x
-		emap(GL_NO_ERROR);
-		emap(GL_INVALID_ENUM);
-		emap(GL_INVALID_VALUE);
-		emap(GL_INVALID_OPERATION);
-		emap(GL_INVALID_FRAMEBUFFER_OPERATION);
-		emap(GL_OUT_OF_MEMORY);
-		default: return "??";
-		#undef emap
+	#define emap(x) case x: return #x
+	emap(GL_NO_ERROR);
+	emap(GL_INVALID_ENUM);
+	emap(GL_INVALID_VALUE);
+	emap(GL_INVALID_OPERATION);
+	emap(GL_INVALID_FRAMEBUFFER_OPERATION);
+	emap(GL_OUT_OF_MEMORY);
+	default: return "??";
+	#undef emap
 	}
 }
 
