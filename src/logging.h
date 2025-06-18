d5efd2facec8540cb4f03095a8a85540a2b21b13
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-06-17 21:39:14 -0500

    logging can now be turned on/off globally

---
+++ src/logging.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glad/glad.h>
+
+inline const char* getErrorName(GLenum error) {
+	switch (error) {
+		#define emap(x) case x: return #x
+		emap(GL_NO_ERROR);
+		emap(GL_INVALID_ENUM);
+		emap(GL_INVALID_VALUE);
+		emap(GL_INVALID_OPERATION);
+		emap(GL_INVALID_FRAMEBUFFER_OPERATION);
+		emap(GL_OUT_OF_MEMORY);
+		default: return "??";
+		#undef emap
+	}
+}
+
+#define LOG(...) printf(__VA_ARGS__)
+#define GLcall(x,...) x(__VA_ARGS__); LOG("glError: %s\n",\
+	getErrorName(glGetError()))
+
+#undef LOG
+#define LOG(...)
