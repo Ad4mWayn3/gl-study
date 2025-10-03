21583bc1c9e81352c78b8a03e4b6482c4abd19ae
Ad4m Wayn3 <misterwayne04@gmail.com>
2025-10-03 10:59:12 -0500

    fixing aspect ratio progress

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,4 +1,5 @@
 #version 330 core
+
 in vec2 texCoord;
 
 out vec4 FragColor;
@@ -6,10 +7,12 @@ out vec4 FragColor;
 uniform sampler2D tex;
 uniform sampler2D tex2;
 uniform float redValue;
+uniform vec2 resolution;
 
 void main() {
 	vec4 texColor1 = texture(tex, texCoord);
 	vec4 texColor2 = texture(tex2, texCoord);
+	vec2 uv = gl_FragCoord.xy / resolution;
 
-	FragColor = mix(texColor1, texColor2, redValue);
+	FragColor = mix(texColor1, texColor2, uv.x);
 }
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -4,17 +4,19 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform float redValue;
+uniform float time;
 uniform vec3 camera;
+uniform vec2 resolution;
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
-out vec3 vertColor;
 out vec2 texCoord;
 
 void main() {
 	mat4 transform = projection * view * model;
 
 	gl_Position = transform * (vec4(aPos, 1.0) + vec4(-.3, -.3, 0., 0.));
+	gl_Position.x *= resolution.y/resolution.x;
 	texCoord = aTexCoord;
 }
